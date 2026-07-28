5e34a5e0e2f2a90a1353237a98cc50aee0bec96e
Cheimasassi <sassiichaima@gmail.com>
2026-07-28 15:00:17 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,11 @@
+if exists (select 1 from sys.databases where name ='DataWarehouse')
+begin
+	alter Database Datawarehouse SET SINGLE8USER WITH ROLLBACK IMMEDIATE ;
+	DROP DATABASE DataWarehouse;
+END;
+Go
+
+
 create database DataWarehouse;
 use DataWarehouse;
 Go
